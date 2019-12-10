67db03205641d091aca65e02aed979d0eb8db218
Stanislav Orlov <stasorlov21@gmail.com>
2019-12-10 23:24:42 +0200

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VladyslavOrlovPromo.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VladyslavOrlovPromo.Startup))]
+namespace VladyslavOrlovPromo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
