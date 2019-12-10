936397548bd340af8237b5a7223e74af6695c3e2
Stanislav Orlov <stasorlov21@gmail.com>
2019-12-10 23:18:12 +0200

    Add files via upload

---
+++ Models/AdminLoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VladyslavOrlovPromo.Models
+{
+    public class AdminLoginModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
