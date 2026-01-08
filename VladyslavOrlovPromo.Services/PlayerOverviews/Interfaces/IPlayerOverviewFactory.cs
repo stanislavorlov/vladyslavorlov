feabc005f1478cd4a8d22a3345f2cfcc86506890
stanislavorlov <stasorlov21@gmail.com>
2026-01-08 13:33:22 +0200

    bootstrap index.html

--- VladyslavOrlovPromo.Services/PlayerOverviews/Interfaces/IPlayerOverviewFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using VladyslavOrlovPromo.Core.Entities;
-
-namespace VladyslavOrlovPromo.Services.PlayerOverviews.Interfaces
-{
-    public interface IPlayerOverviewFactory
-    {
-        PlayerOverview Create(string json);
-    }
-}
--- VladyslavOrlovPromo.Web.NetCore/Models/RankingViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VladyslavOrlovPromo.Web.NetCore.Models
-{
-    public class RankingViewModel
-    {
-        public RankingPartialDisplay Current { get; set; }
-
-        public RankingPartialDisplay Highest { get; set; }
-    }
-}
